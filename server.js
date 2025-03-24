e9d27169bfb4b1597c5ed27eb8c966bf92aa2edb
Barnabas Molnar <barnabas.molnar@gmgcolor.com>
2025-03-24 08:55:44 +0100

    add contact route to the server

--- server.js
+++ server.js
@@ -5,22 +5,48 @@ const http = require('http');
 // creating server
 // reuest and response both are the callback fn param 
 // that are used to node.js queries params body headers and even cookies
-const server  = http.createServer((req, res) => {
+const server = http.createServer((req, res) => {
 
     //handle incoming request
 
-    if(req.url == '/') {
+    if (req.url === '/') {
         //set response headers
-        res.writeHead(200, { 'content-type': 'text/html'})
-        
+        res.writeHead(200, { 'content-type': 'text/html' });
+
         // set response headers
-        res.write('<html><body><p>This is the home page</p></body></html>')
+        res.write('<html><body><p>This is the home page</p></body></html>');
 
         res.end();
     }
-})
 
-server.listen(3000)
+    if (req.url === '/contact') {
+        //set response headers
+        res.writeHead(200, { 'content-type': 'text/html' });
+
+        // set response headers
+        res.write(`
+            <html>
+                <body>
+                    <div>
+                        <h1>This is our contact page</h1>
+                    </div>
+                    <form>
+                        <input type="text" name='email' />
+                        <input type="text" name='password' />
+
+                        <label for="addtional-info">You can write to us here :</label>
+                        <textarea id="addtional-info" name="addtional-info" rows={4} cols={50}  placeholder='Here you can write to us'>
+                        </textarea>
+                </form>
+            </body>
+            </html>`
+        );
+
+        res.end();
+    }
+});
+
+server.listen(3000);
 
 // http//:localhost:3000
-console.log('Node js web server at port 3000 is running')
+console.log('Node js web server at port 3000 is running');
